6bf7b8bd0722b2aecc51c113bf2173eea28049e9
juliahoermedinger <j.hoermedinger@students.htl-leonding.ac.at>
2025-04-29 11:47:46 +0200

    Meine Änderungen am Navbar-Layout

---
+++ navbar.js
@@ -0,0 +1,14 @@
+﻿// SCROLL TOGGLE SCRIPT
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY || window.pageYOffset;
+
+        if (scrollY > 100) {
+            navbar.classList.add("bubble");
+        } else {
+            navbar.classList.remove("bubble");
+        }
+    });
+});
